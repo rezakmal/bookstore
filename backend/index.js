79b015bbe79a91e3d88ca0b0bba3cbd9a533cb6c
Reza Akmal <rezajakmal@gmail.com>
2025-01-04 23:49:28 +0700

    get all books

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDB_URL} from "./config.js";
 import mongoose from 'mongoose';
+import { book } from "./models/bookModel.js";
 
 const app = express();
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome!");
+});
+
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        };
+
+        const book = await book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
 });
 
 mongoose
