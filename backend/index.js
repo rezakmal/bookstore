deec8fbbbc81e02a9bd316ec481e1bd3661f1ab5
Reza Akmal <rezajakmal@gmail.com>
2025-01-05 23:08:16 +0700

    changed the code to save a new book

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,69 @@
 import express, { response } from "express";
 import { PORT, mongoDB_URL} from "./config.js";
 import mongoose from 'mongoose';
-import { book } from "./models/bookModel.js";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// middleware for parsing request body
+app.use(express.json());
+
 app.get('/', (request, response) => {
     console.log(request);
-    return response.status(234).send("Welcome!");
+    return response.status(200).send("Welcome!");
 });
 
 // route for save a new book
-app.post('/books', async (request, response) => {
+// old code:
+// app.post('/books', async (request, response) => {
+//     try {
+//         if (
+//             !request.body.title ||
+//             !request.body.author ||
+//             !request.body.publishYear
+//         ) {
+//             return response.status(400).send({
+//                 message: "Send all required fields: title, author, publishYear",
+//             });
+//         }
+//         const newBook = {
+//             title: request.body.title,
+//             author: request.body.author,
+//             publishYear: request.body.publishYear
+//         };
+
+//         const book = await book.create(newBook);
+
+//         return response.status(201).send(book);
+//     } catch (error) {
+//         console.log(error.message);
+//         response.status(500).send({message: error.message})
+//     }
+// });
+
+// endpoint to save a new book
+app.post("/books", async (request, response) => {
     try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
+        const { title, author, publishYear } = request.body;
+
+        // Validasi input
+        if (!title || !author || !publishYear) {
             return response.status(400).send({
                 message: "Send all required fields: title, author, publishYear",
             });
         }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear
-        };
 
-        const book = await book.create(newBook);
+        // Buat dokumen baru menggunakan model
+        const newBook = new Book({ title, author, publishYear });
 
-        return response.status(201).send(book);
+        // Simpan dokumen ke database
+        const savedBook = await newBook.save();
+
+        // Kirim respons sukses
+        return response.status(201).send(savedBook);
     } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message})
+        console.error("Error:", error.message);
+        return response.status(500).send({ message: error.message });
     }
 });
 
