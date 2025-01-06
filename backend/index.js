6ddf7ecdac9f846e30017028174562d67dd4716e
Reza Akmal <rezajakmal@gmail.com>
2025-01-06 18:21:51 +0700

    created a route for get a book by id

--- backend/index.js
+++ backend/index.js
@@ -81,6 +81,19 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// route for get one book from database by id
+app.get('/books/:id', async (request, response) => {
+    try {
+        const {id} = request.params;
+
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
 mongoose
     .connect(mongoDB_URL)
     .then(() => {
