ff5959c2f8d7cc481c5bc812db2bbc8a82f6dc84
Reza Akmal <rezajakmal@gmail.com>
2025-01-13 17:24:20 +0700

    deleted comment

--- backend/index.js
+++ backend/index.js
@@ -13,34 +13,6 @@ app.get('/', (request, response) => {
     return response.status(200).send("Welcome!");
 });
 
-// route for save a new book
-// old code:
-// app.post('/books', async (request, response) => {
-//     try {
-//         if (
-//             !request.body.title ||
-//             !request.body.author ||
-//             !request.body.publishYear
-//         ) {
-//             return response.status(400).send({
-//                 message: "Send all required fields: title, author, publishYear",
-//             });
-//         }
-//         const newBook = {
-//             title: request.body.title,
-//             author: request.body.author,
-//             publishYear: request.body.publishYear
-//         };
-
-//         const book = await book.create(newBook);
-
-//         return response.status(201).send(book);
-//     } catch (error) {
-//         console.log(error.message);
-//         response.status(500).send({message: error.message})
-//     }
-// });
-
 // endpoint to save a new book
 app.post("/books", async (request, response) => {
     try {
