af0c2e6a0cf08281499eb512f1a8c2ba052f40bf
Reza Akmal <rezajakmal@gmail.com>
2025-01-24 21:46:17 +0700

    add notistack package to show a snackbar

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,15 +1,17 @@
-import React, {useState} from 'react';
-import BackButton from '../components/BackButton';
-import Spinner from '../components/Spinner';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBooks = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishYear, setPublishYear] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueSnackbar } = useSnackbar;
   const handleSaveBook = () => {
     const data = {
       title,
@@ -18,23 +20,25 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post("http://localhost:5555/books", data)
       .then(() => {
         setLoading(false);
-          navigate('/');
+        enqueSnackbar("Book created successfully!", { variant: "success" });
+        navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happended. Please check console');
+        // alert('An error happended. Please check console');
+        enqueSnackbar("Error!", { variant: "error" });
         console.log(error);
       });
   };
 
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <BackButton />
       <h1 className="text-3xl my-4">Create Book</h1>
-      {loading ? <Spinner/> : ''}
+      {loading ? <Spinner /> : ""}
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
@@ -42,7 +46,7 @@ const CreateBooks = () => {
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
 
@@ -52,7 +56,7 @@ const CreateBooks = () => {
             type="text"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
 
@@ -62,16 +66,16 @@ const CreateBooks = () => {
             type="text"
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
 
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
           Save
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateBooks
+export default CreateBooks;
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,29 +1,35 @@
-import React, {useState, useEffect} from 'react';
-import BackButton from '../components/BackButton';
-import Spinner from '../components/Spinner';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const EditBook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishYear, setPublishYear] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const {id} = useParams();
+  const { enqueSnackbar } = useSnackbar;
+  const { id } = useParams();
   useEffect(() => {
     setLoading(true);
-    axios.get(`http://localhost:5555/books/${id}`)
-    .then((response) => {
-      setAuthor(response.data.author);
-      setPublishYear(response.data.publishYear);
-      setTitle(response.data.title);
-      setLoading(false);
-    }).catch((error) => {
-      setLoading(false);
-      alert('An error happened. Please check console.');
-      console.log(error);
-    });
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setAuthor(response.data.author);
+        setPublishYear(response.data.publishYear);
+        setTitle(response.data.title);
+        setLoading(false);
+        enqueSnackbar("Book edited successfully!", { variant: "success" });
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert('An error happened. Please check console.');
+        enqueSnackbar("Error!", { variant: "error" });
+        console.log(error);
+      });
   }, []);
   const handleEditBook = () => {
     const data = {
@@ -36,20 +42,20 @@ const EditBook = () => {
       .put(`http://localhost:5555/books/${id}`, data)
       .then(() => {
         setLoading(false);
-          navigate('/');
+        navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happended. Please check console');
+        alert("An error happended. Please check console");
         console.log(error);
       });
   };
 
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <BackButton />
       <h1 className="text-3xl my-4">Edit Book</h1>
-      {loading ? <Spinner/> : ''}
+      {loading ? <Spinner /> : ""}
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Title</label>
@@ -57,7 +63,7 @@ const EditBook = () => {
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
 
@@ -67,7 +73,7 @@ const EditBook = () => {
             type="text"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
 
@@ -77,16 +83,16 @@ const EditBook = () => {
             type="text"
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
+            className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
 
-        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
           Save
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditBook
+export default EditBook;
